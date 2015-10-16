2c3ea4ef6471b87e13e9ff44b02d7454319c081f
pradeep <pradeep.nagendra@tarento.com>
2015-10-17 00:18:47 +0200

    Added files to Pod classes

---
+++ Example/Pods/Headers/Private/PKCustomAlertView/PKAlertViewController.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/PKAlertViewController.h
--- Example/Pods/Target Support Files/PKCustomAlertView/PKCustomAlertView-umbrella.h
+++ Example/Pods/Target Support Files/PKCustomAlertView/PKCustomAlertView-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "PKAlertModel.h"
+#import "PKAlertViewController.h"
 
 FOUNDATION_EXPORT double PKCustomAlertViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char PKCustomAlertViewVersionString[];
